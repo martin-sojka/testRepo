67c717c85ed2e2fe21f270d883b61742d563aeb5
msojka <martin.sojka@actum.cz>
2018-04-20 15:56:43 +0200

    add Maven

---
+++ codes/src/cz/codes/TestCompare.java
@@ -0,0 +1,48 @@
+package cz.codes;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.mockito.Mockito.*;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class TestCompare {
+	
+	Compare comp = mock(Compare.class);
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testCompareArrayListOfInteger() {
+		
+		ArrayList<Integer> numbers = new ArrayList<Integer>();
+		numbers.add(1);
+		numbers.add(2);
+		numbers.add(3);
+		numbers.add(4);
+		numbers.add(5);
+		
+		
+	}
+
+	@Test
+	public void testCompareArrayListOfIntegerArrayListOfInteger() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testComp() {
+		fail("Not yet implemented");
+	}
+
+}
